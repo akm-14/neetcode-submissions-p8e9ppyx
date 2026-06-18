0703e7777872a69bb3376ec40c0c23291105dace
akm-14 <162075211+akm-14@users.noreply.github.com>
2026-06-18 23:03:00 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.cpp
@@ -0,0 +1,35 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maximum_profit = 0;
+        // for(int i = 0; i < n; i++){
+        //     for (int j = i; j < n; j++){
+        //         int profit = prices[j]-prices[i];
+        //         maximum_profit = max(maximum_profit, profit);
+        //     }
+        // }
+        
+        // vector<int> max_for;
+        // max_for.insert(max_for.begin(), 0);
+        // int maximum_forward = prices[n-1];
+        // for (int i = n-1; i > 0; i--){
+        //     maximum_forward = max(prices[i], maximum_forward);
+        //     max_for.insert(max_for.begin(), maximum_forward);
+            
+        // }
+        // for (int j = 0; j < n; j++){
+        //     int profit = 0;
+        //     profit = max_for[j] - prices[j];
+        //     maximum_profit = max(maximum_profit, profit);
+        // }
+
+        int minimum = prices[0];
+        for (int i = 0; i < n; i++){
+            minimum = min(minimum, prices[i]);
+            maximum_profit = max(maximum_profit, prices[i]-minimum);
+        }
+
+        return maximum_profit;
+    }
+};
